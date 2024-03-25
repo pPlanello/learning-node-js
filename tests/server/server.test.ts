53d64d3eb67418406f5a3950c65788ea65b5fbbc
pPlanello <pplanellodev@gmail.com>
2024-03-25 13:13:16 +0100

    Fix test load environment

--- tests/server/server.test.ts
+++ tests/server/server.test.ts
@@ -2,19 +2,27 @@ import Server from '../../src/server/Server';
 
 describe('Server', () => {
 	let server: Server;
+	const originalEnvironment: NodeJS.ProcessEnv = { ...process.env };
 
 	beforeEach(() => {
-		jest.resetModules();
+		process.env = { ...originalEnvironment };
+	});
+
+	afterEach(() => {
+		process.env = originalEnvironment;
+	});
+
+	it('should return the correct port number when is in environment variable', () => {
 		server = new Server();
+
+		expect(server.getPort()).toBe(3000);
 	});
 
 	it('should return the default port when is not found in environment variable', () => {
 		delete process.env.PORT;
 
-		expect(server.getPort()).toBe(8000);
-	});
+		server = new Server();
 
-	it('should return the correct port number when is in environment variable', () => {
-		expect(server.getPort()).toBe(3000);
+		expect(server.getPort()).toBe(8000);
 	});
 });
